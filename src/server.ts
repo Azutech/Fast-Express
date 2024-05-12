9a392e8944becba3a156f089b819f6511036d352
Azutech <azunna.onugha@gmail.com>
2024-05-12 22:15:49 +0100

    chore () : Server created TSC config updated

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { config } from 'dotenv';
+
+config();
+
+import { PORT } from './utils/config';
+
+const server: Application = express();
+
+server.use(express.json());
+// server.use(helmet());
+
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req: Request, res: Response) => {
+	res.status(StatusCodes.OK).json({ msg: 'Welcome To Fast-Express 🚀🚀' });
+});
+
+server.get('*', (req: Request, res: Response) => {
+	res.status(StatusCodes.NOT_FOUND).json({ message: 'route not found 🔎' });
+});
+
+server.listen(PORT, () => {
+	console.log(`Fast-Express is listening at http://localhost:${PORT} 🚀🚀`);
+});
+
+export default { server };
