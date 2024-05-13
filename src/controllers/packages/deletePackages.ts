b3cf5c8a2d53a866294255a3d64e4f24d5d9721d
Azutech <azunna.onugha@gmail.com>
2024-05-13 05:25:02 +0100

    feature () : Package removal completed

---
+++ src/controllers/packages/deletePackages.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const packageClient = new PrismaClient().package;
+
+
+
+export const deletePackage = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.query;
+
+		if (typeof id !== 'string') {
+			throw new Error('Package ID must be a string');
+		}
+
+		const task = await packageClient.delete({
+			where: {
+				id: id,
+			},
+		});
+
+		if (!task) {
+			throw new Error('Error delete packages');
+		}
+
+		return res
+			.status(StatusCodes.OK)
+			.json({ msg: 'Package deleted successfully', data: task });
+	} catch (err: any) {
+		console.error(err.message);
+		const statusMap: Record<string, number> = {
+			'Error delete packages': StatusCodes.BAD_REQUEST,
+		};
+
+		const statusCode = statusMap[err.message]
+			? statusMap[err.message]
+			: StatusCodes.INTERNAL_SERVER_ERROR;
+		return res.status(statusCode).json({ error: err.message });
+	}
+};
