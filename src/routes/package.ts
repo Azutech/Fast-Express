42094764dd1e7d5eb87ec7e04a6ee0cb1098a21a
Azutech <azunna.onugha@gmail.com>
2024-05-15 09:37:30 +0100

    feat () : Auth Token Created

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { allPackages, createPackage, deletePackage, allPackagesByUser, updatePackage } from '../controllers/packages';
+import { authenticateUser } from '../middlewares/authorizedToken';
 
 export const pack: Router = Router();
 
-pack.post('/createPack', createPackage);
-pack.get('/allPackages', allPackages);
-pack.get('/allPackagesByUser', allPackagesByUser);
-pack.put('/updatePackage', updatePackage)
-pack.delete('/removePackage', deletePackage)
+pack.post('/createPack', authenticateUser, createPackage);
+pack.get('/allPackages', authenticateUser, allPackages);
+pack.get('/allPackagesByUser',  authenticateUser, allPackagesByUser);
+pack.put('/updatePackage', authenticateUser, updatePackage)
+pack.delete('/removePackage', authenticateUser, deletePackage)
